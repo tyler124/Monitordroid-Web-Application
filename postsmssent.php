bc1800b2c624960bb79f037d64929da95a44229e
Tyler Butler <tbutler@bu.edu>
2014-08-24 17:02:31 -0400

    Update postsmssent.php

--- postsmssent.php
+++ postsmssent.php
@@ -1,23 +1,12 @@
     <?php
 
     // receive data from app's http request
-
-    $data= $_POST["SMSSentData"];
-
+    	$data= $_POST["SMSSentData"];
 	$rName= $_POST["regName"];
 
-    // write data from my android app to a text file
-
-   // file_put_contents('myTextFile.txt',$data);
-
- 	//file_put_contents('phoneid.txt', $rName);
-
-	
 
 	include_once './db_functions.php';
-
 	$db = new DB_Functions();
-
 	$db->insertSentSMS($rName, $data);
 
     ?>
