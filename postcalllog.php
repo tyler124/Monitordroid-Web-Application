2d3cfc64566b944ad50c12c6002f8801cb36da29
Tyler Butler <tbutler@bu.edu>
2014-08-24 17:00:22 -0400

    Update postcalllog.php

--- postcalllog.php
+++ postcalllog.php
@@ -1,6 +1,6 @@
     <?php
-    // receive data from app's http request
-    $data= $_POST["LogData"];
+    	// receive data from app's http request
+    	$data= $_POST["LogData"];
 	$rName=$_POST["regName"];
 	
 	include_once './db_functions.php';
